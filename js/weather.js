7641725454c35e763a23eb1e956ca8be6877545a
Jun Seung Hyun <jsh5408@naver.com>
2021-07-12 04:27:35 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log(lat, lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.API_KEY}&units=metric`;
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
