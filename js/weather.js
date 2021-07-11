4e834f8c446cdf3b5f19b1c2b3e42aa73c9adc97
jsh5408 <jsh5408@naver.com>
2021-07-12 03:39:47 +0900

    fix

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,11 @@
-const API_KEY = config.API_KEY;
-
-const weather = document.getElementById("#weather span:first-child");
-const city = document.getElementById("#weather span:last-child");
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log(lat, lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.API_KEY}&units=metric`;
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
