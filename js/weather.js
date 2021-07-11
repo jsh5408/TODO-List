2b2b72eb3ed24e6ed712d4dc14f83a142ce21dae
jsh5408 <jsh5408@naver.com>
2021-07-12 03:35:26 +0900

    update css

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,17 @@
 const API_KEY = config.API_KEY;
 
 const weather = document.getElementById("#weather span:first-child");
-        const city = document.getElementById("#weather span:last-child");
+const city = document.getElementById("#weather span:last-child");
 
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
+    console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then(data => {
-        city.innerText = `@${data.name}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
