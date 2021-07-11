11885f0c8f6216bfcb4637c64871f7102bcc1438
jsh5408 <jsh5408@naver.com>
2021-07-12 04:28:16 +0900

    update weather

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,6 @@ const city = document.querySelector("#weather span:first-child");
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.API_KEY}&units=metric`;
     fetch(url)
     .then((response) => response.json())
